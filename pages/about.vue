d88202a23a188e9ebae8092f6302707142df8aa5
Konstantin Vasin <randomhero177@gmail.com>
2020-12-15 16:35:51 +0300

    add navbar and about page

---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <p>Lorem isadasdasdasdasd </p>
+      <p>Lorem isadasdasdasdasd </p>
+      <p>Lorem isadasdasdasdasd </p>
+      <p>Lorem isadasdasdasdasd </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "about"
+    }
+</script>
+
+<style scoped>
+
+</style>
